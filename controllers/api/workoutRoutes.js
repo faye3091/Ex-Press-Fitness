58d30455806e9be1dcfaaa6e5784903473d2128f
Vanessa <faye_3091@yahoo.com>
2022-12-01 18:13:21 -0500

    changes

--- controllers/api/workoutRoutes.js
+++ controllers/api/workoutRoutes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const { Workout } = require("../../models");
 const withAuth = require("../../utils/auth");
 
-router.post("/", withAuth, async (req, res) => {
+router.get("/", withAuth, async (req, res) => {
   try {
     const newFavorite = await Workout.create({
       ...req.body,
