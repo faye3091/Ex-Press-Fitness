1fa31d26f4b74604e43bcb9cd7512d5ceefeca9d
Vanessa <faye_3091@yahoo.com>
2022-12-05 18:31:13 -0500

    updated homepage

--- controllers/api/workoutRoutes.js
+++ controllers/api/workoutRoutes.js
@@ -20,16 +20,16 @@ router.get("/workout", withAuth, async (req, res) => {
         console.log(err);
         res.status(500).json(err);
     });
-  try {
-    const newFavorite = await Workout.create({
-      ...req.body,
-      user_id: req.session.user_id,
-    });
+  // try {
+  //   const newFavorite = await Workout.create({
+  //     ...req.body,
+  //     user_id: req.session.user_id,
+  //   });
 
-    res.status(200).json(newFavorite);
-  } catch (err) {
-    res.status(400).json(err);
-  }
+  //   res.status(200).json(newFavorite);
+//   } catch (err) {
+//     res.status(400).json(err);
+//   }
 });
 
 router.delete("/:id", withAuth, async (req, res) => {
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -30,27 +30,27 @@ router.get("/", async (req, res) => {
   // }
 });
 
-router.get("/workout/:id", async (req, res) => {
-  try {
-    const workoutData = await Workout.findByPk(req.params.id, {
-      include: [
-        {
-          model: User,
-          attributes: ["name"],
-        },
-      ],
-    });
+// router.get("/workout/:id", async (req, res) => {
+//   try {
+//     const workoutData = await Workout.findByPk(req.params.id, {
+//       include: [
+//         {
+//           model: User,
+//           attributes: ["name"],
+//         },
+//       ],
+//     });
 
-    const workout = workoutData.get({ plain: true });
+//     const workout = workoutData.get({ plain: true });
 
-    res.render("workout", {
-      ...workout,
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+//     res.render("workout", {
+//       ...workout,
+//       logged_in: req.session.logged_in,
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
 // Use withAuth middleware to prevent access to route
 router.get("/profile", withAuth, async (req, res) => {
